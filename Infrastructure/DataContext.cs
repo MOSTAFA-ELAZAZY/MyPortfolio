811a4adf1498a649b741abc22b43dc85aa6506ad
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-16 00:27:47 +0200

    aaa

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+   public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options )
+            :base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().HasData(new Owner
+            {
+                FullName = "Mostafa Elazazy",
+                Avatar = "mostafa.jpg",
+                Profil = ".Net Developer"
+
+            });
+        }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -12,5 +12,11 @@ namespace Web.Controllers
         {
             return View();
         }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
